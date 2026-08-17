b71afe8e44c6fcbda7bd5ddfb832ecb79fcc37bf
ChaosSpeed <fclhaamoes@gmail.com>
2026-08-17 21:59:50 +0530

    Fix config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ export const config = {
   FORM_ACTION_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSei2mdeE4l-Irv43MG3sQga7bVCarYwSHJgqzD0dUypUgMyBQ/formResponse',
   FORM_ENTRY_ID_NAME: 'entry.441265152',
   FORM_ENTRY_ID_URL: 'entry.1871454451',
-  SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/1bAzy-rwAVRqgra3QqHlOv_-xQT5Pt5lqbaOFl_IH6FE/eexport?format=csv&gid=2078442075'
+  SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/1bAzy-rwAVRqgra3QqHlOv_-xQT5Pt5lqbaOFl_IH6FE/export?format=csv&gid=2078442075'
 };
