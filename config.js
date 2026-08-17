80d78ba2bcbca0a18caf613b65accdd7ae483397
Chaos0Speed <fclhaamoes@gmail.com>
2026-08-17 21:51:57 +0530

    updating test

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ export const config = {
   FORM_ACTION_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSei2mdeE4l-Irv43MG3sQga7bVCarYwSHJgqzD0dUypUgMyBQ/formResponse',
   FORM_ENTRY_ID_NAME: 'entry.441265152',
   FORM_ENTRY_ID_URL: 'entry.1871454451',
-  SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNIrbp0g6WwEtIUGd3XpI1mTIjoc0efMgvHK-UxFy2Yw8LkFcr4IUvPruwSzgpbkZNof_vgHy5KA9Y/pub?gid=2078442075&single=true&output=csv'
+  SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/1bAzy-rwAVRqgra3QqHlOv_-xQT5Pt5lqbaOFl_IH6FE/eexport?format=csv&gid=2078442075'
 };
