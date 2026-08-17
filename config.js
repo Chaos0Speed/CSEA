6b572bef30181a87b0a4ea94ff998a4bfb9f2f35
Chaos0Speed <fclhaamoes@gmail.com>
2026-08-17 16:30:52 +0530

    updating test

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+  IMGBB_API_KEY: 'ffa4d27769b766c270856e930fa92d43',
+  FORM_ACTION_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSei2mdeE4l-Irv43MG3sQga7bVCarYwSHJgqzD0dUypUgMyBQ/formResponse',
+  FORM_ENTRY_ID_NAME: 'entry.441265152',
+  FORM_ENTRY_ID_URL: 'entry.1871454451',
+  SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNIrbp0g6WwEtIUGd3XpI1mTIjoc0efMgvHK-UxFy2Yw8LkFcr4IUvPruwSzgpbkZNof_vgHy5KA9Y/pub?gid=2078442075&single=true&output=csv'
+};
